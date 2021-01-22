1b5212836328674fdfece0b5cb316ebfc27d76d5
Devdreamsolution <q3construction1@gmail.com>
2021-01-22 22:52:36 +1000

    add validation catch

--- app/Http/Controllers/API/CybersourceController.php
+++ app/Http/Controllers/API/CybersourceController.php
@@ -41,86 +41,93 @@ class CybersourceController extends BaseController
      */
     public function postCheckout(Request $request)
     {
-        $validated = $this->validate($request, [
-            'number' => ['required', 'int'],
-            'expiration_month' => ['required', 'int'],
-            'expiration_year' => ['required', 'int'],
-            'total_amount' => ['required'],
-            'currency' => ['required'],
-            'first_name' => ['required'],
-            'last_name' => ['required'],
-            'address1' => ['required'],
-            'locality' => ['required'],
-            'administrative_area' => ['required'],
-            'postal_code' => ['required'],
-            'country' => ['required'],
-            'email' => ['required'],
-            'phone_number' => ['required']
-        ]);
-        $this->flag === true ? $capture = true : $capture = false;
+        try {
+            $validated = $this->validate($request, [
+                'number' => ['required', 'int'],
+                'expiration_month' => ['required', 'int'],
+                'expiration_year' => ['required', 'int'],
+                'total_amount' => ['required'],
+                'currency' => ['required'],
+                'first_name' => ['required'],
+                'last_name' => ['required'],
+                'address1' => ['required'],
+                'locality' => ['required'],
+                'administrative_area' => ['required'],
+                'postal_code' => ['required'],
+                'country' => ['required'],
+                'email' => ['required'],
+                'phone_number' => ['required']
+            ]);
+        } catch (ValidationException $validationException) {
+            return $this->sendError($validationException->getMessage(), $validationException->errors());
+        }
+
+        try {
+            $this->flag === true ? $capture = true : $capture = false;
+            $clientReferenceInformationArr = [
+                "code" => time()
+            ];
+            $clientReferenceInformation = new Ptsv2paymentsClientReferenceInformation($clientReferenceInformationArr);
         
-        $clientReferenceInformationArr = [
-            "code" => 'TC50171_3'
-        ];
-        $clientReferenceInformation = new Ptsv2paymentsClientReferenceInformation($clientReferenceInformationArr);
-    
-        $processingInformationArr = [
-            "capture" => $capture
-        ];
-        $processingInformation = new Ptsv2paymentsProcessingInformation($processingInformationArr);
-    
-        $paymentInformationCardArr = [
-            "number" => $validated['number'],
-            "expirationMonth" => $validated['expiration_month'],
-            "expirationYear" => $validated['expiration_year']
-        ];
-        $paymentInformationCard = new Ptsv2paymentsPaymentInformationCard($paymentInformationCardArr);
-    
-        $paymentInformationArr = [
-            "card" => $paymentInformationCard
-        ];
-        $paymentInformation = new Ptsv2paymentsPaymentInformation($paymentInformationArr);
-    
-        $orderInformationAmountDetailsArr = [
-            "totalAmount" => $validated['total_amount'],
-            "currency" => $validated['currency']
-        ];
-        $orderInformationAmountDetails = new Ptsv2paymentsOrderInformationAmountDetails($orderInformationAmountDetailsArr);
-    
-        $orderInformationBillToArr = [
-            "firstName" => $validated['first_name'],
-            "lastName" => $validated['last_name'],
-            "address1" => $validated['address1'],
-            "locality" => $validated['locality'],
-            "administrativeArea" => $validated['administrative_area'],
-            "postalCode" => $validated['postal_code'],
-            "country" => $validated['country'],
-            "email" => $validated['email'],
-            "phoneNumber" => $validated['phone_number']
-        ];
-        $orderInformationBillTo = new Ptsv2paymentsOrderInformationBillTo($orderInformationBillToArr);
-    
-        $orderInformationArr = [
-            "amountDetails" => $orderInformationAmountDetails,
-            "billTo" => $orderInformationBillTo
-        ];
-        $orderInformation = new Ptsv2paymentsOrderInformation($orderInformationArr);
-    
-        $requestObjArr = [
-            "clientReferenceInformation" => $clientReferenceInformation,
-            "processingInformation" => $processingInformation,
-            "paymentInformation" => $paymentInformation,
-            "orderInformation" => $orderInformation
-        ];
-        $requestObj = new CreatePaymentRequest($requestObjArr);
-    
-        $commonElement = new ExternalConfiguration();
-        $config = $commonElement->ConnectionHost();
-        $merchantConfig = $commonElement->merchantConfigObject();
-    
-        $api_client = new ApiClient($config, $merchantConfig);
-        $api_instance = new PaymentsApi($api_client);
-    
+            $processingInformationArr = [
+                "capture" => $capture
+            ];
+            $processingInformation = new Ptsv2paymentsProcessingInformation($processingInformationArr);
+        
+            $paymentInformationCardArr = [
+                "number" => $validated['number'],
+                "expirationMonth" => $validated['expiration_month'],
+                "expirationYear" => $validated['expiration_year']
+            ];
+            $paymentInformationCard = new Ptsv2paymentsPaymentInformationCard($paymentInformationCardArr);
+        
+            $paymentInformationArr = [
+                "card" => $paymentInformationCard
+            ];
+            $paymentInformation = new Ptsv2paymentsPaymentInformation($paymentInformationArr);
+        
+            $orderInformationAmountDetailsArr = [
+                "totalAmount" => $validated['total_amount'],
+                "currency" => $validated['currency']
+            ];
+            $orderInformationAmountDetails = new Ptsv2paymentsOrderInformationAmountDetails($orderInformationAmountDetailsArr);
+        
+            $orderInformationBillToArr = [
+                "firstName" => $validated['first_name'],
+                "lastName" => $validated['last_name'],
+                "address1" => $validated['address1'],
+                "locality" => $validated['locality'],
+                "administrativeArea" => $validated['administrative_area'],
+                "postalCode" => $validated['postal_code'],
+                "country" => $validated['country'],
+                "email" => $validated['email'],
+                "phoneNumber" => $validated['phone_number']
+            ];
+            $orderInformationBillTo = new Ptsv2paymentsOrderInformationBillTo($orderInformationBillToArr);
+        
+            $orderInformationArr = [
+                "amountDetails" => $orderInformationAmountDetails,
+                "billTo" => $orderInformationBillTo
+            ];
+            $orderInformation = new Ptsv2paymentsOrderInformation($orderInformationArr);
+        
+            $requestObjArr = [
+                "clientReferenceInformation" => $clientReferenceInformation,
+                "processingInformation" => $processingInformation,
+                "paymentInformation" => $paymentInformation,
+                "orderInformation" => $orderInformation
+            ];
+            $requestObj = new CreatePaymentRequest($requestObjArr);
+        
+            $commonElement = new ExternalConfiguration();
+            $config = $commonElement->ConnectionHost();
+            $merchantConfig = $commonElement->merchantConfigObject();
+        
+            $api_client = new ApiClient($config, $merchantConfig);
+            $api_instance = new PaymentsApi($api_client);
+        } catch (Exception $exception) {
+            return $this->sendError($exception->getMessage());
+        }
         try {
             $apiResponse = $api_instance->createPayment($requestObj);
     
