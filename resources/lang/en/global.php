9ff0d4162fa550b2ec0e9193a7e25661927a2ce4
Devdreamsolution <q3construction1@gmail.com>
2021-02-10 04:32:21 +1000

    Auth: login and register page style, user model path fix in register model

---
+++ resources/lang/en/global.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+	'login' => [
+		'title' => 'Login',
+		'formDescription' => 'Login to Your Account',
+		'email' => 'Email',
+		'password' => 'Password',
+		'rememberMe' => 'Remember me',
+		'signIn' => 'SIGN IN',
+		'forgotPassword' => 'Forgot password?',
+		'signUp' => 'Sign Up',
+		'notHaveAccountYet' => "Don't have an account yet?",
+		'joinOurCommunity' => 'JOIN OUR COMMUNITY',
+		'description' => 'Magazine Heaven Direct is the specialist partner for enterprising, independent magazine publishers. Whether you are an existing magazine or yet to launch, we will help you achieve your circulation goals and provide a range of specialist, tailored services that will really benefit your business. We pride ourselves on the quality and range of our services and deliver unique, creative solutions for publishers who need to get their product to market or expand and develop an existing market or distribution channel, either in the UK or Internationally. Magazine Heaven Direct is affiliated with Magazine Heaven, The UK’s largest Magazine Retail store, stocking more 3600 publications, with an impressive e-commerce store and offering worldwide single copy sales direct to consumer and subscriptions. Magazine Heaven also curates and sells back issues for many publishers and offers mailing services and bulk deliveries.'
+	],
+	'register' => [
+		'title' => 'Register',
+		'formDescription' => 'Create an Account',
+		'yourName' => 'Your name',
+		'yourEmail' => 'Your email',
+		'confirmPassword' => 'Confirm password',
+		'agreeTerms' => 'By clicking "Sign Up", you agree to our',
+		'termsAndPolicy' => 'terms of service and privacy policy',
+		'willSendEmail' => 'We’ll occasionally send you account related emails.',
+		'haveAccount' => 'Already have an account?'
+	]
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,169 @@
-@extends('layouts.app')
+<!DOCTYPE html>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<html lang="en" class="default-style">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+<head>
+    <title>{{ __('global.login.title') }} - {{ config('app.name') }}</title>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900" rel="stylesheet">
+
+    <!-- Icon fonts -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/fontawesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/ionicons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/linearicons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/open-iconic.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/pe-icon-7-stroke.css') }}">
+
+    <!-- Core stylesheets -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/bootstrap.css') }}" class="theme-settings-bootstrap-css">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/appwork.css') }}" class="theme-settings-appwork-css">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/theme-corporate.css') }}" class="theme-settings-theme-css">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/colors.css') }}" class="theme-settings-colors-css">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/uikit.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}">
+
+    <!-- Load polyfills -->
+    <script src="{{ asset('assets/vendor/js/polyfills.js') }}"></script>
+    <script>document['documentMode']===10&&document.write('<script src="https://polyfill.io/v3/polyfill.min.js?features=Intl.~locale.en"><\/script>')</script>
+
+    <script src="{{ asset('assets/vendor/js/material-ripple.js') }}"></script>
+    <script src="{{ asset('assets/vendor/js/layout-helpers.js') }}"></script>
+
+    <!-- Theme settings -->
+    <!-- This file MUST be included after core stylesheets and layout-helpers.js in the <head> section -->
+    <script src="{{ asset('assets/vendor/js/theme-settings.js') }}"></script>
+    <script>
+        window.themeSettings = new ThemeSettings({
+        cssPath: "{{ asset('assets/vendor/css/rtl') }}/",
+        themesPath: "{{ asset('assets/vendor/css/rtl') }}/"
+        });
+    </script>
+
+    <!-- Core scripts -->
+    <script src="{{ asset('assets/vendor/js/pace.js') }}"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- Libs -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}">
+    <!-- Page -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/authentication.css') }}">
+</head>
+
+<body>
+    <div class="page-loader">
+        <div class="bg-primary"></div>
+    </div>
+
+    <!-- Content -->
+
+    <div class="authentication-wrapper authentication-3">
+        <div class="authentication-inner">
+
+        <!-- Side container -->
+        <!-- Do not display the container on extra small, small and medium screens -->
+        <div class="d-none d-lg-flex col-lg-8 align-items-center ui-bg-cover ui-bg-overlay-container p-5" style="background-image: url('assets/img/bg/21.jpg');">
+            <div class="ui-bg-overlay bg-dark opacity-50"></div>
+
+            <!-- Text -->
+            <div class="w-100 text-white px-5">
+            <h1 class="display-2 font-weight-bolder mb-4">{{ __('global.login.joinOurCommunity') }}</h1>
+            <div class="text-large font-weight-light">
+                {{ __('global.login.description') }}
+            </div>
+            </div>
+            <!-- /.Text -->
+        </div>
+        <!-- / Side container -->
+
+        <!-- Form container -->
+        <div class="d-flex col-lg-4 align-items-center bg-white p-5">
+            <!-- Inner container -->
+            <!-- Have to add `.d-flex` to control width via `.col-*` classes -->
+            <div class="d-flex col-sm-7 col-md-5 col-lg-12 px-0 px-xl-4 mx-auto">
+                <div class="w-100">
+
+                <!-- Logo -->
+                <div class="d-flex justify-content-center align-items-center">
+                    <div class="ui-w-60">
+                        <div class="w-100 position-relative" style="padding-bottom: 54%">
+                        <svg class="w-100 h-100 position-absolute" viewBox="0 0 148 80" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><linearGradient id="a" x1="46.49" x2="62.46" y1="53.39" y2="48.2" gradientUnits="userSpaceOnUse"><stop stop-opacity=".25" offset="0"></stop><stop stop-opacity=".1" offset=".3"></stop><stop stop-opacity="0" offset=".9"></stop></linearGradient><linearGradient id="e" x1="76.9" x2="92.64" y1="26.38" y2="31.49" xlink:href="#a"></linearGradient><linearGradient id="d" x1="107.12" x2="122.74" y1="53.41" y2="48.33" xlink:href="#a"></linearGradient></defs><path class="fill-primary" transform="translate(-.1)" d="M121.36,0,104.42,45.08,88.71,3.28A5.09,5.09,0,0,0,83.93,0H64.27A5.09,5.09,0,0,0,59.5,3.28L43.79,45.08,26.85,0H.1L29.43,76.74A5.09,5.09,0,0,0,34.19,80H53.39a5.09,5.09,0,0,0,4.77-3.26L74.1,35l16,41.74A5.09,5.09,0,0,0,94.82,80h18.95a5.09,5.09,0,0,0,4.76-3.24L148.1,0Z"></path><path transform="translate(-.1)" d="M52.19,22.73l-8.4,22.35L56.51,78.94a5,5,0,0,0,1.64-2.19l7.34-19.2Z" fill="url(#a)"></path><path transform="translate(-.1)" d="M95.73,22l-7-18.69a5,5,0,0,0-1.64-2.21L74.1,35l8.33,21.79Z" fill="url(#e)"></path><path transform="translate(-.1)" d="M112.73,23l-8.31,22.12,12.66,33.7a5,5,0,0,0,1.45-2l7.3-18.93Z" fill="url(#d)"></path></svg>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
+                <!-- / Logo -->
+
+                <h4 class="text-center text-lighter font-weight-normal mt-5 mb-0">{{ __('global.login.formDescription') }}</h4>
+
+                <!-- Form -->
+                <form class="my-5" method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label class="form-label" for="email">{{ __('global.login.email') }}</label>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label d-flex justify-content-between align-items-end" for="password">
+                        <div>{{ __('global.login.password') }}</div>
+                        <a href="{{ route('password.request') }}" class="d-block small">{{ __('global.login.forgotPassword') }}</a>
+                        </label>
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center m-0">
+                        <label class="custom-control custom-checkbox m-0" for="remember">
+                        <input name="remember" type="checkbox" class="custom-control-input" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <span class="custom-control-label">{{ __('global.login.rememberMe') }}</span>
+                        </label>
+                        <button type="submit" class="btn btn-primary">{{ __('global.login.signIn') }}</button>
+                    </div>
+                </form>
+                <!-- / Form -->
+
+                <div class="text-center text-muted">
+                    {{ __('global.login.notHaveAccountYet') }} <a href="{{ route('register') }}">{{ __('global.login.signUp') }}</a>
+                </div>
+
+            </div>
             </div>
         </div>
+        <!-- / Form container -->
+
+        </div>
     </div>
-</div>
-@endsection
+
+    <!-- / Content -->
+
+    <!-- Core scripts -->
+    <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('assets/vendor/js/sidenav.js') }}"></script>
+
+    <!-- Libs -->
+    <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+
+    <!-- Demo -->
+    <script src="{{ asset('assets/js/demo.js') }}"></script>
+
+</body>
+
+</html>
