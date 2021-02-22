fa545c279d1f47b188d406f1a3a0c196036f1f6a
Devdreamsolution <q3construction1@gmail.com>
2021-02-22 23:57:35 +1000

    change genre select2 min-height to 350px

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,6 +75,12 @@
 
 @push('css')
 <link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2.css') }}">
+<style>
+	.select2-results__options {
+		min-height: 350px;
+		overflow-y: auto;
+	}
+</style>
 @endpush
 
 @push('js')
