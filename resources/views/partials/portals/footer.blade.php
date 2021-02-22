0538c585ec58a78ef3573d7650bae9e0264bc529
Devdreamsolution <q3construction1@gmail.com>
2021-02-23 00:07:16 +1000

    remove site map menu

--- resources/views/partials/portals/footer.blade.php
+++ resources/views/partials/portals/footer.blade.php
@@ -11,7 +11,7 @@
 			<a href="{{ route('contact-us.view') }}" class="footer-link pt-3 ml-4">{{ __('global.contactUs.title') }}</a>
 			<a href="javascript:void(0)" class="footer-link pt-3 ml-4">{{ __('global.termsAndConditions.title') }}</a>
 			<a href="javascript:void(0)" class="footer-link pt-3 ml-4">{{ __('global.privacyPolicy.title') }}</a>
-			<a href="javascript:void(0)" class="footer-link pt-3 ml-4">{{ __('global.siteMap.title') }}</a>
+			{{-- <a href="javascript:void(0)" class="footer-link pt-3 ml-4">{{ __('global.siteMap.title') }}</a> --}}
 		</div>
 	</div>
 </nav>
--- resources/views/partials/websites/footer.blade.php
+++ resources/views/partials/websites/footer.blade.php
@@ -7,7 +7,7 @@
 		<a href="{{ route('contact-us.view') }}" class="footer-link pt-3 ml-4">{{ __('global.contactUs.title') }}</a>
 		<a href="javascript:void(0)" class="footer-link pt-3 ml-4">{{ __('global.termsAndConditions.title') }}</a>
 		<a href="javascript:void(0)" class="footer-link pt-3 ml-4">{{ __('global.privacyPolicy.title') }}</a>
-		<a href="javascript:void(0)" class="footer-link pt-3 ml-4">{{ __('global.siteMap.title') }}</a>
+		{{-- <a href="javascript:void(0)" class="footer-link pt-3 ml-4">{{ __('global.siteMap.title') }}</a> --}}
 	</div>
 
 	<hr class="d-none d-lg-block w-100 my-2">
